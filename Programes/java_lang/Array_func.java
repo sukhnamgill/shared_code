ba6d4cd704d5e5f6c45050be2a4a92d3cbcebd92
Sukhnam <sukhnam23@gmail.com>
2025-08-10 09:52:30 +0530

    library system
    java new feature for  library system

---
+++ Programes/java_lang/Array_func.java
@@ -0,0 +1,15 @@
+public class Array_func{
+
+    static void add(int[] arr,int value){
+
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("Array function to add and eliminate the value");
+        int[] num=new int[5];
+        // num.length=num.length+1;
+        num[3]=5;
+        System.out.println(num[3]);
+    }
+}
--- Programes/java_lang/Practice_set_2.java
+++ Programes/java_lang/Practice_set_2.java
@@ -1,15 +1,33 @@
 class Library{
-    int len=1;
-    String[] books={"English"};
+    int len=0;
+    String [] books=new String[10];
+    //adding books
     void add_book(String name){
         books[len]=name;
-        System.out.println("Value is added in array"+name);
+        System.out.println("Value is added in array :->"+ name);
         len=len+1;
+        
     }
+    //printing books list
     void print_details(){
+        System.out.println("_______________\nList of books");
         for (String i :books){
-        System.out.println(i);}
+            if (i!= null){
+        System.out.println("-> "+i);}}
     }
+    //removing books
+    void remove_book (String name){
+        int indx=0;
+        for(String i :books){
+            if(i==name){
+                books[indx]=null;
+                System.out.println(name +" is deletd from book list");
+                indx=indx+1;
+            }
+            
+        }
+    }
+
 }
 
 
@@ -20,6 +38,9 @@ public class Practice_set_2{
         System.out.println("Sukhnam singh gill");
         Library s1=new Library();
         s1.add_book("Hindi");
+        s1.add_book("english");
+        s1.remove_book("Hindi");
+
         // s1.add_book("French");
         s1.print_details();
     }
