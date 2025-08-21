e31a93a2a4eb2901c52a280f6adb9051e4f4fe97
Sukhnam <sukhnam23@gmail.com>
2025-08-21 19:08:52 +0530

    newne

---
+++ Programes/Data Structure And Algorthims/vectr.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void reverse(vector<int> &vc){
+    for (int i = 0; i < vc.size()/2; i++)
+    {
+     swap(vc[i],vc[vc.size()-i-1]);
+    }
+    
+}
+int main(){
+    cout<<"iam sukhnam";
+    vector<int> vec={1,2,3};
+    cout<<vec[1];
+    vec.push_back(10);
+    cout<<endl<<vec.at(3)<<endl;
+    cout<<vec.size();
+    cout<<"printing array"<<endl;
+    reverse(vec);
+    for(auto i:vec){
+        cout<<i<<endl;
+    }
+    
+    //printing size of vector
+
+
+
+
+
+    return 0;
+}
