8351eebad797c981ded5ef26b680951118c61941
Sukhnam <sukhnam23@gmail.com>
2025-09-09 19:22:49 +0530

    pull

--- Programes/Data Structure And Algorthims/waterCantainer.cpp
+++ Programes/Data Structure And Algorthims/waterCantainer.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
-int main(){
+//brute force approach
+void bigWater(int arr[],int len){
+    
+    for(int i=0;i<len;i++){
+        int area=0;
+        
+        for(int j=i;j<len;j++){
+            area++;
+            area=area+arr[i]+arr[j];
 
-    cout<<"hello world"<<endl;
+        }
+        cout<<area<<endl;
+    }
+}
+
+int main(){
+    //programe to find the bigger cantainer
+    cout<<"hello world";
+    int water[]={1,4,2,4,2,6,7,8,5,4};
+    int len=sizeof(water)/sizeof(water[0]);
+    bigWater(water,len);
+
+
+
+
     return 0;
 }
